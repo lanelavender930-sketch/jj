4fd6e7ee3832b952d9f93d46ee44e13a9ce54792
lanelavender930-sketch <lanelavender930@gmail.com>
2026-01-31 17:46:55 +0100

    FAQs page added

--- script-nursery.js
+++ script-nursery.js
@@ -1,3 +1,4 @@
+/*opinions grid*/
 const opinionCards = document.querySelectorAll('.opinions-grid');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -13,4 +14,17 @@ const observer = new IntersectionObserver((entries) => {
 });
 opinionCards.forEach(grid => {
   observer.observe(grid);
+});
+/*drop list questions*/
+document.querySelectorAll(".faq-question").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const answer = btn.nextElementSibling;
+
+btn.classList.toggle("active");
+
+    answer.style.height =
+      answer.style.height === "0px" || !answer.style.height
+        ? answer.scrollHeight + "px"
+        : "0px";
+  });
 });
